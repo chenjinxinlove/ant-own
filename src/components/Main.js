2ebbe1a07ad3b33dfe10fbaeab60931475edb53e
陈金鑫 <505150704@qq.com>
2017-03-14 15:29:12 +0800

    init

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+
+  }
+  render() {
+    return (
+     <div></div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+
+export default AppComponent
